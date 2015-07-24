71d13652e94a995662614845d67c2f9993e9f154
TheSake <sauli.hanninen@hotmail.fi>
2015-07-24 11:04:23 +0300

    game over screen

--- Assets/Script/UI_manager.cs
+++ Assets/Script/UI_manager.cs
@@ -2,13 +2,16 @@
 using System.Collections;
 
 public class UI_manager : MonoBehaviour {
+
+	public GameObject selectLevelPanel;
+	public GameObject gameOverScreen;
+
 	public void StartGame(){
 		Application.LoadLevel ("1_game");
 	}
 	public void QuitGame(){
 		Application.Quit ();
 	}
-	public GameObject selectLevelPanel;
 	public void showSelectLevelPanel(){
 		selectLevelPanel.SetActive (true);
 	}
@@ -17,7 +20,11 @@ public class UI_manager : MonoBehaviour {
 	}
 	// Use this for initialization
 	void Start () {
-		selectLevelPanel.SetActive (false);
+		if (Application.loadedLevelName.Equals ("0_menu")) {
+			selectLevelPanel.SetActive (false);
+		}else if(Application.loadedLevelName.Equals ("1_game")){
+			gameOverScreen.SetActive (false);
+		}
 	}
 	
 	// Update is called once per frame
