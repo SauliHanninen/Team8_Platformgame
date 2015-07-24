57bfb2dcc38836890e3e33a53c3b528ab223db74
unknown <odaov001@MDMA325W07.ksamk.ad>
2015-07-24 12:37:26 +0300

    Here's the minimum viable product

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+	void Awake(){
+
+
+	}
+	// A static field accessible by code anywhere
+	static public bool goalMet = false;
+	void OnTriggerEnter( Collider other ) {
+		// When the trigger is hit by something
+		// Check to see if it's a Projectile
+		if ( other.gameObject.tag == "Soldier" ) {
+			print ("you won!");
+			int score=PlayerPrefs.GetInt("BestScore");
+
+			// If so, set goalMet to true
+			Goal.goalMet = true;
+			// Also set the alpha of the color to higher opacity
+			Color c = GetComponent<Renderer>().material.color;
+			c.a = 1;
+			GetComponent<Renderer>().material.color = c;
+		}
+	}
+}
--- Assets/Script/Soldier.cs
+++ Assets/Script/Soldier.cs
@@ -8,12 +8,21 @@ public class Soldier : MonoBehaviour {
 	public static int lives;
 	private bool isFalling;
 
+	//projectile features
+	public GameObject prefabProjectile;
+	//public Vector3 launchPos;
+	public GameObject projectile;
+	public float velocityMult = 6f;
+
 	// Use this for initialization
 	void Start () {
 
 		lives = 3;
 	}
-	
+
+	void Awake(){
+		//launchPos = transform.position;
+	}
 	// Update is called once per frame
 	void Update () {
 
@@ -28,12 +37,25 @@ public class Soldier : MonoBehaviour {
 			soldierPosition.x+=0.1F;
 			this.transform.position=soldierPosition;
 		}
-
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			// Instantiate a projectile
+			projectile = Instantiate (prefabProjectile) as GameObject;
+			// Start it at the launch point
+			projectile.transform.position = transform.position;
+			// Set it to isKinematic for now
+			projectile.GetComponent<Rigidbody>().isKinematic = true;
+			projectile.transform.Translate(Time.deltaTime*velocityMult,0,0); 
+			//projectile.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+			//transform.Translate(Vector3.forward Time.deltaTime velocityMult);;
+		}
 
 		if (Input.GetKeyDown(KeyCode.UpArrow) && isFalling == false) {
 			GetComponent<Rigidbody>().AddForce(new Vector3 (0, 500, 0));
 			isFalling = true;
 		}
+
+
+
 
 	}
 
