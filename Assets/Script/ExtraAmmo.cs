568122bd9fb9a1811bf7ea2d1a966898425a2be6
TheSake <sauli.hanninen@hotmail.fi>
2015-07-28 12:39:15 +0300

    collectable ammo prehab and script that will give player ammo when he/she picks it up

---
+++ Assets/Script/ExtraAmmo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraAmmo : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider){
+		GameObject collidedWith = collider.gameObject;
+		if (collidedWith.CompareTag ("Soldier")) {
+			Soldier.ammo += 5;
+			Destroy(gameObject);
+		}
+	}
+}
